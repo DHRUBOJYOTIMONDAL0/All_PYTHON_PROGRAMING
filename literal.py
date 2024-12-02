189ba261229bbab05119b9139fef97aad471138e
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2024-12-02 21:39:10 +0530

    inital command

---
+++ literal.py
@@ -0,0 +1,4 @@
+character = "C";
+character = chr(ord(character)+1); # ord(character): Converts the character to its ASCII value.
+#ord(character) + 1: Adds 1 to the ASCII value to get the next character in the sequence.
+print (character);
