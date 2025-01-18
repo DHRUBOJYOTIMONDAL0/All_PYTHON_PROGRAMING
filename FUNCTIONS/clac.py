4a9469dc513d5354af1098fad53ddd4c524b2b59
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-18 14:45:11 +0530

    basic function

---
+++ FUNCTIONS/clac.py
@@ -0,0 +1,15 @@
+# function definition
+# def clac_sum(a, b): # parameters
+#     # sum = a+b
+#     # print(sum)
+#     # return sum
+#     return a+b
+# print(clac_sum(5, 10)) # funcction call and arguments
+# print(clac_sum(2, 10)) 
+# print(clac_sum(12, 17))    
+
+
+def print_hello():
+    print("Hello World")
+output = print_hello()
+print(output) # None  
