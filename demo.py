e9a390e9fb7b7475fe9187a678dc69d053f2c494
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-18 12:49:43 +0530

    All functions in one folder

---
+++ demo.py
@@ -0,0 +1,13 @@
+# list=['abcd',786,2.23,'john',70.2]
+# tinylist=[123,'john']
+# print(list)
+# print(list[0])
+# print(list[1:3])
+# print(list[2:])
+# print(tinylist*2)
+# print(list+tinylist)
+# del list[2]
+# list1.append("c#")
+list=[10,20,30,40,50]
+print(list)
+print(list.index(30))
